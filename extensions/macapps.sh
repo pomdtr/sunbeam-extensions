d5aafd878f46edf2f38b90de3bdb9ff0658a7186
pomdtr <achille.lacoin@gmail.com>
2023-10-26 23:30:54 +0200

    add macapps script

---
+++ extensions/macapps.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+set -eu
+
+if [ $# -eq 0 ]; then
+    sunbeam query -n '{
+        title: "Mac Apps",
+        commands: [
+            {
+                name: "list",
+                title: "List All Apps",
+                mode: "view"
+            }
+        ]
+    }'
+    exit 0
+fi
+
+if [ "$1" = "list" ]; then
+    mdfind kMDItemContentTypeTree=com.apple.application-bundle -onlyin / | sunbeam query -R '{
+        title: (split("/")[-1]),
+        actions: [{
+            title: "Open",
+            onAction: {
+                type: "open",
+                target: .,
+                exit: true
+            }
+        }]
+    }' | sunbeam query -s '{ type: "list", items: . }'
+fi
